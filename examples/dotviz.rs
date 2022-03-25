88459a7c7b97abf128a607d5d3e5c8f4c1429779
Masterchef365 <duncan.freeman1@gmail.com>
2022-03-25 02:58:11 -0700

    Optional labels

--- examples/dotviz.rs
+++ examples/dotviz.rs
@@ -15,6 +15,10 @@ struct Opt {
     #[structopt(short, long)]
     no_zeros: bool,
 
+    /// Include labels
+    #[structopt(short, long)]
+    labels: bool,
+
     /// Output file path, defaults to stdout
     #[structopt(short, long)]
     out_path: Option<PathBuf>,
@@ -34,21 +38,35 @@ fn main() -> Result<()> {
     life.insert_array(&rle, rle_width, (half_width, half_width), n);
 
     if let Some(out_path) = args.out_path {
-        write_child_graph(&life, BufWriter::new(File::create(out_path)?), args.no_zeros)?;
+        write_child_graph(
+            &life,
+            BufWriter::new(File::create(out_path)?),
+            args.no_zeros,
+            args.labels,
+        )?;
     } else {
-        write_child_graph(&life, std::io::stdout(), args.no_zeros)?;
+        write_child_graph(&life, std::io::stdout(), args.no_zeros, args.labels)?;
     }
 
     Ok(())
 }
 
-fn write_child_graph<W: Write>(life: &HashLife, mut f: W, omit_zeros: bool) -> io::Result<()> {
+fn write_child_graph<W: Write>(
+    life: &HashLife,
+    mut f: W,
+    omit_zeros: bool,
+    labels: bool,
+) -> io::Result<()> {
     writeln!(f, "strict digraph {{")?;
     for (idx, cell) in life.cells().iter().enumerate() {
         let names = ["TL", "TR", "BL", "BR"];
         for (child, name) in cell.children.into_iter().zip(names) {
             if !((child.id() == 0) && omit_zeros) {
-                writeln!(f, "    {} -> {} [label=\"{}\"]", idx, child.id(), name)?;
+                if labels {
+                    writeln!(f, "    {} -> {} [label=\"{}\"]", idx, child.id(), name)?;
+                } else {
+                    writeln!(f, "    {} -> {}", idx, child.id())?;
+                }
             }
         }
     }
